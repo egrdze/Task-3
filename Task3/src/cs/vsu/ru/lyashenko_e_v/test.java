9d6fc2d938deaff2cfa025ce68cfeba33def24aa
egrdze <62994823+egrdze@users.noreply.github.com>
2021-12-22 17:10:17 +0300

    Update test.java

--- Task3/src/cs/vsu/ru/lyashenko_e_v/test.java
+++ Task3/src/cs/vsu/ru/lyashenko_e_v/test.java
@@ -7,23 +7,23 @@ package cs.vsu.ru.lyashenko_e_v;
                 System.out.println("Test 1 is not completed");
                 return false;
             }
-            if (Picture.getColor(4, -1) != SimpleColor.WHITE){
+            if (Picture.getColor(4, -1) != SimpleColor.WHITE) {
                 System.out.println("Test 2 is not completed");
                 return false;
             }
-            if (Picture.getColor(5, 1) != SimpleColor.BLUE){
+            if (Picture.getColor(5, 1) != SimpleColor.BLUE) {
                 System.out.println("Test 3 is not completed");
                 return false;
             }
-            if (Picture.getColor(-4, 1) != SimpleColor.BLUE){
+            if (Picture.getColor(-4, 1) != SimpleColor.BLUE) {
                 System.out.println("Test 4 is not completed");
                 return false;
             }
-            if (Picture.getColor(-2, -4) != SimpleColor.GRAY){
+            if (Picture.getColor(-2, -4) != SimpleColor.GRAY) {
                 System.out.println("Test 5 is not completed");
                 return false;
             }
-            if (Picture.getColor(-2, -6) != SimpleColor.GRAY){
+            if (Picture.getColor(-2, -6) != SimpleColor.GRAY) {
                 System.out.println("Test 6 is not completed");
                 return false;
 
