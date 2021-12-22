45a0a714b509137ada461e41d91f485507ad93bb
egrdze <62994823+egrdze@users.noreply.github.com>
2021-12-22 17:35:46 +0300

    Update Picture.java

--- Task3/src/cs/vsu/ru/lyashenko_e_v/Picture.java
+++ Task3/src/cs/vsu/ru/lyashenko_e_v/Picture.java
@@ -1,11 +1,11 @@
 package cs.vsu.ru.lyashenko_e_v;
 
 public class Picture {
-    private static final HorizontalParabola P1 = new HorizontalParabola(4, -3, -0.25);
-    private static final shapeCircle C1 = new shapeCircle(1, -3, 4);
-    private static final shapeCircle C2 = new shapeCircle(-2, -5, 2);
+    private final HorizontalParabola P1 = new HorizontalParabola(4, -3, -0.25);
+    private final shapeCircle C1 = new shapeCircle(1, -3, 4);
+    private final shapeCircle C2 = new shapeCircle(-2, -5, 2);
 
-    private static SimpleColor getColor(double x, double y) {
+    public SimpleColor getColor(double x, double y) {
         if (P1.isPointLeftOfParabola(x, y)) {
             if (C1.insideCoord(x, y)) {
                 if (C2.insideCoord(x, y)) {
