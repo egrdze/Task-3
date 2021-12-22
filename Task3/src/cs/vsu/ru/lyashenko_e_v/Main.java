c77e7b6f0a66956e2c1116b67af9c6fb695620e9
egrdze <62994823+egrdze@users.noreply.github.com>
2021-12-22 17:10:36 +0300

    Update Main.java

--- Task3/src/cs/vsu/ru/lyashenko_e_v/Main.java
+++ Task3/src/cs/vsu/ru/lyashenko_e_v/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (test.testProgram()){
+        if (test.testProgram()) {
             System.out.println(("Test error"));
             System.exit(1);
         } else {
